0ddb486cd6832fc5009d565fc6080510a1f410e6
Pedejeca135 <pjco90@hotmail.com>
2021-12-08 10:46:14 -0600

    primeros pasos con el BOM

---
+++ JS/js/29-BOM.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//BOM - Browser object model
+function getBom(){
+    console.log(window.innerHeight);
+    console.log(window.innerWidth);
+    console.log(window.location);
+}
+
+function redirect(url){
+    window.location.href = url;
+}
+
+function abrirVentana(url){
+    window.open(url,"","width=400,height=500");
+}
+
+abrirVentana("youtube.com");
+
+getBom();
