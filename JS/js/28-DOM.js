43e70cb9a44ea945c6d6795b77e082a08aca3195
PedroCantu-Dev <pedro.cantu_dev@outlook.com>
2021-12-08 08:55:16 -0600

    primeros pasos con el dom

---
+++ JS/js/28-DOM.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//DOM: Document Object Model
+
+function cambiaColor(color){
+    caja.style.color = color;
+}
+
+var caja = document.getElementById("micaja");
+
+var caje = document.querySelector("#micaja")
+
+caja.style.backgroundColor = "red";
+caja.style.padding = "20px";
+caja.style.color = "#ccc";
+caja.className = "hola";
+console.log(caje );
+
