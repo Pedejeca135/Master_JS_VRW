2eebf8ff0486894bc30281fd1b615ca6612b7aa0
Pedejeca135 <pjco90@hotmail.com>
2021-12-09 21:14:15 -0600

    fetch + json

--- JS/js/36-fetch.js
+++ JS/js/36-fetch.js
@@ -1,11 +1,21 @@
 'use strict'
 
 //Fetch y peticiones a servicios / apis rest
+var div_usuarios = document.querySelector("#usuarios");
 var usuarios = [];
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://reqres.in/api/users')
     .then(data => data.json())
-    .then(data => {
-        usuarios = data;
+    .then(users => {
+        usuarios = users.data;
         console.log(usuarios);
+
+        usuarios.map((user,i)=>{
+
+            let nombre = document.createElement('h3');
+
+            nombre.innerHTML = i + '.'+ user.first_name + " "+  user.last_name;
+
+            div_usuarios.appendChild(nombre);
+        });
     });
