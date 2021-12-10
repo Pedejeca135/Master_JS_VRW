4525063d09d6b2a05de90f391a10a73853a98791
Pedejeca135 <pjco90@hotmail.com>
2021-12-09 22:53:00 -0600

    fetch po

--- JS/js/36-fetch.js
+++ JS/js/36-fetch.js
@@ -6,10 +6,24 @@ var usuarios = [];
 
 fetch('https://reqres.in/api/users')
     .then(data => data.json())
-    .then(users => {
-        usuarios = users.data;
-        console.log(usuarios);
 
+    .then(users => {
+
+        listadoUsuarios(users.data);
+        return getUser();
+        console.log(usuarios);
+    });
+
+    function getUsuarios(){
+        return fetch('https://reqres.in/api/users');
+    }
+
+    function getUser(){
+        return fetch("https://reqres.in/api/users")
+    }
+
+    function listadoUsuarios(usuarios)
+    {
         usuarios.map((user,i)=>{
 
             let nombre = document.createElement('h3');
@@ -17,5 +31,9 @@ fetch('https://reqres.in/api/users')
             nombre.innerHTML = i + '.'+ user.first_name + " "+  user.last_name;
 
             div_usuarios.appendChild(nombre);
+
+            document.querySelector(".loading").style.display="none";
         });
-    });
+    }   
+
+  
