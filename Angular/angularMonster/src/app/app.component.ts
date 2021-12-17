dd76199ee9a1095f8994e88b72da8bf06861485a
Pedejeca135 <pjco90@hotmail.com>
2021-12-17 09:47:46 -0600

    no se como controlar las ganas de saber todo a la vez y poder llevar a cabo todas mis ideas, lo siento

--- Angular/angularMonster/src/app/app.component.ts
+++ Angular/angularMonster/src/app/app.component.ts
@@ -1,6 +1,8 @@
 /*Serie de imports */
 import { Component } from '@angular/core';
 
+import { configuracion } from './models/configuration';
+
 //Decorador
 @Component({
   selector: 'app-root',
@@ -9,7 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularMonster The one and only';
+  public descripcion:string;
   public mostrar_videojuegos:boolean = true;
+  public config = configuracion;
+
+  constructor(){
+    this.title = configuracion.titulo;
+    this.descripcion = configuracion.descripcion;
+  }
 
   ocultarVideoJuegos(value:boolean=true){
     this.mostrar_videojuegos = value;
---
+++ Angular/angularMonster/src/app/models/configuration.ts
@@ -0,0 +1,6 @@
+export var configuracion = {
+    color: "red",
+    fondo: "blue",
+    titulo: "Master en Java Script y Angular",
+    descripcion: "aprendiendo angular con victor robles"
+};
